0d1bf1d74533a794beebac72f38d21bc2d731fda
justicewa <1092828478@qq.com>
2020-02-11 21:01:26 +0800

    谢了两天的

--- 2020.02.03/review/review/Program.cpp
+++ 2020.02.03/review/review/Program.cpp
@@ -536,7 +536,7 @@ int main()
 
 #endif
 
-#if 1
+#if 0
 //0711 1
 
 class Board
@@ -558,8 +558,233 @@ public:
 				return true;
 		}
 
+		for (i = 0; i < row; i++)
+		{
+			sum = 0;
+			for (j = 0; j < row; j++)
+			{
+				sum += board[j][i];
+			}
+			if (sum == row)
+				return true;
+		}
+		
+		sum = 0;
+		for (i = 0; i < row; i++)
+		{
+			sum += board[i][i];
+		}
+		if (sum == row)
+			return true;
+
+		sum = 0;
+		for (i = 0; i < row; i++)
+		{
+			sum += board[i][row - i - 1];
+		}
+		if (sum == row)
+			return true;
+
+		return false;
 
 	}
 };
+
+#endif
+
+#if 0
+//0711 2
+
+int numChar(string str, int k)
+{
+	//根据ASCII码判断字母大小写
+	int small = 0;
+	int big = 0;
+	for (int i = 0; i < k; i++)
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+			big++;
+		else if (str[i] >= 97 && str[i] <= 122)
+			small++;
+	}
+	if ((small + big) == 0)
+		return 0;
+	else if (small == k || big == k)
+		return 10;
+	else if (small > 0 && big > 0)
+		return 20;
+	return 0;
+}
+
+int numNunber(string str, int k)
+{
+	//根据ASCII码判断数字个数,减去字符'0'之后在0~9之间的即为数字
+	int num = 0;
+	for (int i = 0; i < k; i++)
+	{
+		if (str[i] - '0' >= 0 && str[i] - '0' <= 9)
+			num++;
+	}
+	if (num == 0)
+		return 0;
+	else if (num == 1)
+		return 10;
+	else
+		return 20;
+}
+
+int numSymbal(string str, int k)
+{
+	int num = 0;
+	for (int i = 0; i < k; i++)
+	{
+		//除去字母, 数字,其它都为符号
+		if (!(str[i] >= 65 && str[i] <= 90)
+			&& !(str[i] >= 97 && str[i] <= 122)
+			&& !(str[i] - '0' >= 0 && str[i] - '0' <= 9))
+			num++;
+	}
+	if (num == 0)
+		return 0;
+	else if (num == 1)
+		return 10;
+	else
+		return 25;
+}
+
+int main()
+{
+	string str;
+	while (cin >> str)
+	{
+		int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0;
+		int k = str.size();
+		if (k <= 4)
+			sum1 = 5;
+		else if (k > 8)
+			sum1 = 5;
+		else sum1 = 10;
+		sum2 = numChar(str, k);
+		sum3 = numNunber1(str, k);
+		sum4 = numSymbal(str, k);
+		if ((sum2 > 0) && (sum3 > 0) && (sum4 > 0))
+		{
+			if (sum2 == 10)
+				sum5 = 3;
+			else
+				sum5 = 5;
+		}
+		else if (sum2 > 0 && sum3 > 0 && sum4 == 0)
+			sum5 = 2;
+		if (sum1 + sum2 + sum3 + sum4 + sum5 >= 90)
+			cout << "VERY_SERCURE" << endl;
+		else if (sum1 + sum2 + sum3 + sum4 + sum5 >= 80)
+			cout << "SEVURE" << endl;
+		else if (sum1 + sum2 + sum3 + sum4 + sum5 >= 70)
+			cout << "VERY_STRONG" << endl;
+		else if (sum1+sum2+sum3+sum4+sum5>=60)
+			cout << "STRONG" << endl;
+		else if (sum1 + sum2 + sum3 + sum4 + sum5 >= 50)
+			cout << "AVERAGE" << endl;
+		else if (sum1 + sum2 + sum3 + sum4 + sum5 >= 25)
+			cout << "WEAK" << endl;
+		else
+			cout << "VERY_WEAK" << endl;
+	}
+	return 0;
+}
+
+#endif
+
+#if 0
+//0712 1
+
+class LCA
+{
+public:
+	static int getLCA(int a, int b)
+	{
+		while (a != b)
+		{
+			if (a > b)
+				a /= 2;
+			else
+				b /= 2;
+		}
+		return a;
+	}
+};
+
+#endif
+
+#if 0
+//0712 2
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		int count = 0, maxCount = 0;
+		while (n)
+		{
+			if (n & 1)
+			{
+				++count;
+				maxCount = max(count, maxCount);
+			}
+			else
+				count = 0;
+			n = n >> 1;
+		}
+		cout << maxCount << endl;
+		return 0;
+	}
+}
+
+#endif
+
+#if 0
+//0713 1
+
+class BinInsert
+{
+public:
+	static int binInsert(int n, int m, int j, int i)
+	{
+		m <<= j;
+		return n | m;
+	}
+};
+
+int main()
+{
+	cout << BinInsert::binInsert(1024, 19, 2, 6);
+
+	return 0;
+}
+
+#endif
+
+#if 1
+//0713 2
+
+bool isPrime(int num)
+{
+	int tmp = sqrt(num);
+	for (int i = 2; i <= tmp; i++)
+	{
+		if (num%i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	
+
+	return 0;
+}
 
 #endif
